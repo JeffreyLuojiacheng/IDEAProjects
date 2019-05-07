cc2f0186363fa376e312b28a4e2497594c400465
jchenluo <jchenluo@163.com>
2019-05-07 13:40:26 +0800

    测试4

--- src/a1_50/a003/Test.java
+++ src/a1_50/a003/Test.java
@@ -3,5 +3,6 @@ package a1_50.a003;
 public class Test {
     public static void main(String[] args) {
         System.out.println("测试3");
+        System.out.println("hhhh3");
     }
 }
--- src/a1_50/a004/Test4.java
+++ src/a1_50/a004/Test4.java
@@ -3,5 +3,6 @@ package a1_50.a004;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("hhh4");
+        System.out.println("hhhh4");
     }
 }
